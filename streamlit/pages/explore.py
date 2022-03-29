eaa520175e98cef0b1f4be13078fcb78a486d131
dafiqrahman <46261435+dafiqrahman@users.noreply.github.com>
2022-03-30 01:38:27 +0700

    Update explore.py

--- streamlit/pages/explore.py
+++ streamlit/pages/explore.py
@@ -7,9 +7,9 @@ import seaborn as sns
 
 def app():
     def file_selector(folder_path='.'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
+        filenames = os.listdir(folder_path)
+        selected_filename = st.selectbox('Select a file', filenames)
+        return os.path.join(folder_path, selected_filename)
     filename = file_selector()
     st.write('You selected `%s`' % filename)
         st.markdown("<h3 style = 'color : #ff4b4b'> Model</h3>",
